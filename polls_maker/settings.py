24cb5b80fad05f26f5ec5c3c8475352660c8c6aa
katszwn <kat.szwn@gmail.com>
2015-01-01 18:21:46 +0100

    Add support for PostgreSQL database

--- polls_maker/settings.py
+++ polls_maker/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'polls_maker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'polls_maker',
+        'USER': 'polls',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
