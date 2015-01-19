4ae60ff878ba791d28c0f38e3a099577b28c4506
katszwn <kat.szwn@gmail.com>
2015-01-19 01:52:51 +0100

    Add choices to admin interface

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['question_text', 'pub_date']
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
