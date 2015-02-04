c60810bd8291a317e12f0db077858640431fce31
katszwn <kat.szwn@gmail.com>
2015-02-04 22:06:50 +0100

    Add basic views, define urls

--- polls/admin.py
+++ polls/admin.py
@@ -8,7 +8,11 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     fields = ['question_text', 'pub_date']
     inlines = [ChoiceInline]
+
     list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
